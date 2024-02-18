b1f00ffbcb3d9e6b9489e697e12e413b563611a2
Logan Helms <logan.a.helms@gmail.com>
2024-02-18 15:04:49 -0500

    Add config and setup

--- lib/ecto_starter/application.ex
+++ lib/ecto_starter/application.ex
@@ -8,8 +8,7 @@ defmodule EctoStarter.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: EctoStarter.Worker.start_link(arg)
-      # {EctoStarter.Worker, arg}
+      EctoStarter.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
